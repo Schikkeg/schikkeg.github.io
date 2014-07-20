c35815a8842477612ff903c1781cfb6c6e6f8a90
Sham <schikkeg@gmail.com>
2014-07-20 19:21:51 +0100

    Initial commit

---
+++ js/aemstuff.js
@@ -0,0 +1,51 @@
+function renderTable(){
+
+
+$(document).ready(function() {
+    var table = $('#example').DataTable({
+    	paging: false,
+    	"autoWidth": false,
+		"scrollY": 280,
+        "scrollX": true,
+        "columnDefs": [
+            { "visible": false, "targets": 2 }
+        ],
+        "order": [[ 2, 'asc' ]],
+        "displayLength": 2225,
+        "drawCallback": function ( settings ) {
+            var api = this.api();
+            var rows = api.rows( {page:'current'} ).nodes();
+            var last=null;
+ 
+            api.column(2, {page:'current'} ).data().each( function ( group, i ) {
+                if ( last !== group ) {
+                    $(rows).eq( i ).before(
+                        '<tr class="group" ><td colspan="5">'+group+'</td></tr>'
+                    );
+ 
+                    last = group;
+                }
+            } );
+        }
+    } );
+    
+    
+ 
+    // Order by the grouping
+    $('#example tbody').on( 'click', 'tr.group', function () {
+        var currentOrder = table.order()[0];
+        if ( currentOrder[0] === 2 && currentOrder[1] === 'asc' ) {
+            table.order( [ 2, 'desc' ] ).draw();
+        }
+        else {
+            table.order( [ 2, 'asc' ] ).draw();
+        }
+    } );
+} );
+}
+
+function drawChart(){
+$(document).ready(function() {
+  $('table.highchart').highchartTable();
+});
+}
